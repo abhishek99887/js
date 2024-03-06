2e1a5c0f44e17e5e5c1e5d6a8de1edba41091b5e
abhishek99887 <162042634+abhishek99887@users.noreply.github.com>
2024-03-06 18:01:49 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treats all JS code as newer JS version
+
+ //alert( 3 + 3) // we are using JS, not browser
+
+console.log(3 
++
+ 3); // vode readablity should be high 
+
+ console.log("Abhi")
+
+
+ let name = "Abhi"
+ let age = 18
+ let isLoggedIn = false
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined => the value is not assigned
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof undefined) // o/p - undefined because it is a type itself 
+console.log(typeof null) // o/p - object 
