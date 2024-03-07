1d62a4778b8667d00471dc551e70d3778b10caba
abhishek99887 <162042634+abhishek99887@users.noreply.github.com>
2024-03-07 15:18:25 +0000

    connversion of datatypes

---
+++ .devcontainer/01_basic/01_conversionOperaton.js
@@ -0,0 +1,28 @@
+let score = "44abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueIntNumber = Number(score)
+//console.log(typeof valueIntNumber)
+//console.log(valueIntNumber)
+
+
+// "33 => 33"
+// ""33abc => NaN (Not a NUmber)
+//true = 1 ; false =0;
+
+let isLoggedIn = "abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abhi" => true
+
+let SomeNumber =  13
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
