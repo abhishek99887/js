06c771647ab75ca4bdd53ba566d9bca4dc50a2fa
abhishek99887 <162042634+abhishek99887@users.noreply.github.com>
2024-03-08 17:14:59 +0000

    datatype  conversion

--- .devcontainer/01_basic/01_conversionOperaton.js
+++ .devcontainer/01_basic/01_conversionOperaton.js
@@ -24,5 +24,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let SomeNumber =  13
 
 let StringNumber = String(SomeNumber)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// ****************operations****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) //2 is to  2 power 
+// console.log(2/2)  
+// console.log(2%2)  //reminder
+
+let str1 = "Hello"
+let str2 = " Abhi"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2") // the string is in last so it will do the addition first and then string is added 
+
+//this is not correct form to write ....use parathesis.
+//if the is string is in first then it will convert into string 
+
+// console.log( ( 3 + 4) * 5 % 3) 
+
+// console.log(+true)
+// console.log(+"") 
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2 + 3 //wrong way to write
+
+let gameCounter = 100
+++gameCounter 
+console.log(gameCounter)
