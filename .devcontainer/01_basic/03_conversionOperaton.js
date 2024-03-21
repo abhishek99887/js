7d4b57cae91eb789d6e2390cc87d5dd1cbee7a93
abhishek99887 <162042634+abhishek99887@users.noreply.github.com>
2024-03-21 18:36:48 +0000

    01_basics

---
+++ .devcontainer/01_basic/03_conversionOperaton.js
@@ -0,0 +1,68 @@
+let score = "44abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueIntNumber = Number(score)
+//console.log(typeof valueIntNumber)
+//console.log(valueIntNumber)
+
+
+// "33 => 33"
+// ""33abc => NaN (Not a NUmber)
+//true = 1 ; false =0;
+
+let isLoggedIn = "abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abhi" => true
+
+let SomeNumber =  13
+
+let StringNumber = String(SomeNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// ****************operations****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) //2 is to  2 power 
+// console.log(2/2)  
+// console.log(2%2)  //reminder
+
+let str1 = "Hello"
+let str2 = " Abhi"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2") // the string is in last so it will do the addition first and then string is added 
+
+//this is not correct form to write ....use parathesis.
+//if the is string is in first then it will convert into string 
+
+// console.log( ( 3 + 4) * 5 % 3) 
+
+// console.log(+true)
+// console.log(+"") 
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2 + 3 //wrong way to write
+
+let gameCounter = 100
+++gameCounter 
+console.log(gameCounter)
---
+++ .devcontainer/01_basic/04_commparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" < 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);  // (avoid these type of comparisons)
+// console.log(null != 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);  // all undefined comparision is false  (avoid these type of comparisons)
+
+//=== :- represents strictly check
+
+console.log("2" == 2);
